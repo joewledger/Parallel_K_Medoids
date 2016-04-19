b8e0f3a352d9c809954a997ca4f3b11d71e2dfb1
joewledger <joewledger@gmail.com>
2016-04-18 22:45:06 -0400

    Reading data out of input file complete.
    Signed-off-by: joewledger <joewledger@gmail.com>

--- Src/k_medoids.c
+++ Src/k_medoids.c
@@ -3,64 +3,85 @@
 #include <string.h>
 #include <sys/time.h>
 
-typedef struct DataPoint {
-    int numFeatures;
-    double* features;
-    char* classification;
-} dp;
+#define nf 4
+#define nd 150
+#define k 5
 
-void readDatapoints(char* filename, dp* datapoints,int numDatapoints, int numFeatures) {
-    //struct DataPoint *datapoints;
-    //datapoints = (dp*) malloc(sizeof(dp) * numDatapoints);
-}
+struct DataPoint {
+    double features[nf];
+    char classification[20];
+};
+
+struct Cluster {
+    int numDatapoints;
+    struct DataPoint *medoid;
+    struct DataPoint *datapoints[nd];
+};
+
+struct DataPoint datapoints[nd];
+struct Cluster clusters[k];
+
+char *tokens;
+char *array[nf + 1];
+double features[nf];
 
 
-
-int main(){
+void readDatapoints(char* filename) {
     FILE *file;
     char * line = NULL;
     int lineNumber = 0;
-    int numDatapoints = 150;
-    int numFeatures = 4;
+    int i = 0;
     size_t len = 0;
     ssize_t read;
-
-    struct DataPoint *datapoints;
-    datapoints = (dp*) malloc(sizeof(dp) * numDatapoints);
-
-    file = fopen("../Data/iris.data", "r");
+    
+    file = fopen(filename, "r");
     if(file){
         while((read = getline(&line,&len,file)) != -1){
-
-            //Create a new Struct representing a datapoint and put it into the array.
-            struct DataPoint dp1;
-            datapoints[lineNumber] = dp1;
-
-            int i = 0;
-            char *tokens = strtok(line,",");
-            char *array[numFeatures];
-            double features[numFeatures - 1];
-
+            tokens = strtok(line,",");
+            
             while(tokens != NULL){
                 array[i++] = tokens;
                 tokens = strtok(NULL,",");
             }
 
-            for(i=0;i<numFeatures;i++){
+            for(i=0;i<nf;i++){
                 features[i] = atof(array[i]);
             }
             
-            datapoints[lineNumber].features = features;
-            datapoints[lineNumber].classification = strtok(array[numFeatures],"\n");
+            memcpy(datapoints[lineNumber].features,features,sizeof(double) * nf);
+            strcpy(datapoints[lineNumber].classification,strtok(array[nf],"\n"));
             lineNumber++;
+            i = 0;
         }
         fclose(file);
     } else {
         printf("File doesn't exist.\n");
     }
-    
-    int i;
-    for(i = 0;i<numDatapoints;i++){
-       printf("%s\n", datapoints[i].classification);
+}
+
+void cluster() {
+    return 0;
+}
+
+void saveClustersToFile() {
+    return 0;
+}
+
+void printDatapoints() {
+    int i,j;
+    for(i = 0; i < nd; i++){
+        for(j = 0; j < nf; j++){
+            printf("%f\t", datapoints[i].features[j]);
+        }
+        printf("%s\n", datapoints[i].classification);
     }
+
+}
+
+int main(){  
+    
+    readDatapoints("../Data/iris.data");
+    printDatapoints();
+    cluster();
+    saveClustersToFile();
 }
