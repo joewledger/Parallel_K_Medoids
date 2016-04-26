3229f2db0a9c19e7b9012e13888ec22e77629258
joewledger <joewledger@gmail.com>
2016-04-26 15:33:39 -0400

    Fixed text file redirection in run.sh
    Signed-off-by: joewledger <joewledger@gmail.com>

--- Src/run.sh
+++ Src/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm k_medoids.o
-gcc -fopenmp k_medoids.c -lm -o k_medoids.o -D CARS >> regular_time.txt
-./k_medoids.o
-gcc -fopenmp k_medoids.c -lm -o k_medoids.o -D IRIS >> regular_time.txt
-./k_medoids.o
-gcc -fopenmp k_medoids.c -lm -o k_medoids.o -D SOYBEAN >> regular_time.txt
-./k_medoids.o
+gcc -fopenmp k_medoids.c -lm -o k_medoids.o -D CARS
+./k_medoids.o >> regular_time.txt
+gcc -fopenmp k_medoids.c -lm -o k_medoids.o -D IRIS
+./k_medoids.o >> regular_time.txt
+gcc -fopenmp k_medoids.c -lm -o k_medoids.o -D SOYBEAN
+./k_medoids.o >> regular_time.txt
